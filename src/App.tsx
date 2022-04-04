3568b175c54272e6a6d291385d3fe65ff6ecbc00
laurpe <laura.perttila@edu.bc.fi>
2022-04-04 12:40:41 +0300

    typescript setup + fetch general typing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
-import React from "react";
+import Card from "./components/Card";
+
+const getData = async <T,>(url: string): Promise<T> => {
+    const response = await fetch(url);
+    const data = response.json() as Promise<T>;
+    return data;
+};
 
 function App() {
-    return <div></div>;
+    const data = getData("data.json");
+    console.log(data);
+    return (
+        <div>
+            <Card />
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/Card.tsx
@@ -0,0 +1,5 @@
+const Card = () => {
+    return <div>sdfsfd</div>;
+};
+
+export default Card;
