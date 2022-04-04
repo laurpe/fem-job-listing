c17c5b40c458786a44850f8f9426211e7337e560
laurpe <laura.perttila@edu.bc.fi>
2022-04-04 15:09:30 +0300

    types.ts added

---
+++ src/types.ts
@@ -0,0 +1,15 @@
+export interface JobData {
+    company: string;
+    contract: string;
+    featured: boolean;
+    id: number;
+    languages: string[];
+    level: string;
+    location: string;
+    logo: string;
+    new: boolean;
+    position: string;
+    postedAt: string;
+    role: string;
+    tools: string[];
+}
