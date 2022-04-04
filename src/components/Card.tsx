eac8dd771e65fad769f6ea22e3e5c00e3e93addd
laurpe <laura.perttila@edu.bc.fi>
2022-04-04 15:11:47 +0300

    card component html base

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,5 +1,35 @@
-const Card = () => {
-    return <div>sdfsfd</div>;
+import { JobData } from "../types";
+
+interface Props {
+    job: JobData;
+}
+
+const Card = ({ job }: Props) => {
+    return (
+        <div>
+            <div>
+                <img src={job.logo} alt="company logo" />
+            </div>
+            <div>
+                <div>
+                    <h3>{job.company}</h3>
+                </div>
+                <div>
+                    <h2>{job.position}</h2>
+                </div>
+                <ul>
+                    <li>{job.postedAt}</li>
+                    <li>{job.contract}</li>
+                    <li>{job.location}</li>
+                </ul>
+            </div>
+            <div>
+                {job.languages.map((item) => (
+                    <div key={item}>{item}</div>
+                ))}
+            </div>
+        </div>
+    );
 };
 
 export default Card;
